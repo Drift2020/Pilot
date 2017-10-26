4b689689711a575b83b18c8bcb69ea907c680e5f
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-10-26 20:52:04 +0300

    Bag
    1. name dispecher
    2. delite dispecher
    3. print

--- Airplane pilot simulator/24-7/Dispatcher.cs
+++ Airplane pilot simulator/24-7/Dispatcher.cs
@@ -11,56 +11,91 @@ namespace _24_7
     {
         public string Name { get; set; }
         public int N { get; set; }
+
+
         public int Score { get; set; }
-       public Dispatcher():this("Elva",150)
+        public bool Quest { get; set; }
+        public bool Life_Air { get; set; }
+        public bool Flight_start { get; set; }
+      
+
+        public Dispatcher():this("Elva",150)
        {
           
        }
-
         public Dispatcher(string Name)
         {
             Random t = new Random();
             this.Name = Name;
+            Quest = false;
+            Flight_start = false;
             N = t.Next(-200, 200);
         }
         public Dispatcher(string Name, int N)
         {
             this.Name = Name;
             this.N = N;
+            Quest = false;
+            Flight_start = false;
         }
-
-        public void Print(int speed, int height)
-        {
-            int recommended_height = 7 * speed - N;
-            Console.WriteLine("Name: {0} / Recomend height: {1}",Name, recommended_height);
-        }
+     
 
         public void Flight_check(int speed, int height)
         {
             int recommended_height = 7 * speed - N;
+
+            if (Flight_start)
+            {
+                Console.WriteLine("Name: {0} / Recomend height: {1}", Name, recommended_height);
+            }
           
-            if()
+            if (   ((height- recommended_height > 300 && height - recommended_height>=0)    && (height - recommended_height <= 600 && height - recommended_height >= 0)) 
+                && ((height - recommended_height > -300 && height - recommended_height < 0) && (height - recommended_height <= -600 && height - recommended_height < 0))
+                && Flight_start)
+            {
+                Score += 25;
+            }
+            //else if((height - recommended_height > 600|| height - recommended_height > -600) && (height - recommended_height <= 1000|| height - recommended_height <= -1000) 
+            //    && Flight_start )
+            //{
+            //    Score += 50;
+            //}
+            else if((height - recommended_height >1000) && Flight_start)
+            {              
+                throw new Exception("The plane crashed!");
+            }
+
+            if(Score>=1000 && Flight_start)
+            {
+                throw new Exception("You are not suitable for flights!");
+            }
+           
+            if(speed>1000)
+            {
+                Score += 100;
+                Console.WriteLine("Slow down the speed!");
+            }
+            if (speed==1000)
+            {
+                Quest = true;
+               
+            }
+            if (!Flight_start&&speed>=50 && !Quest)
+            {
+                Flight_start = !Flight_start;
+            }
+
+            if (Quest&& Flight_start && speed <= 0 && height <= 0)
+            {
+                Flight_start = false;
+                throw new Exception("Aircraft is planted!");
+            }
+            else if(Flight_start && (speed <= 0&&height<=0))
+            {
+                throw new Exception("The plane crashed!");
+            }
+            
         }
     }
 }
-//• Если разница в диапазоне от 300 до 600 то пилот получает
-//25 штрафных очков, если от 600 до 1000 — то 50 очков.
-//• Если разница превышает 1000, то объект-диспетчер
-//генерирует исключительную ситуацию «Самолет
-//разбился
-//», которая должна быть обработана приложением,
-//как прекращение тренировочного полета
-//с соответствующей
-//информацией на экране.
-//• Если пилот, не завершив полет, набирает 1000 штрафных
-//очков от любого диспетчера — то этот объект диспетчер
-//генерирует исключительную ситуацию «Непригоден
-//к полетам», которая также обрабатывается приложением.
-//• Так же недопустимо, чтобы самолет в любой момент
-//времени имел нулевую высоту и нулевую скорость
-//(если это случилось — исключение «Самолет разбился»
-//генерирует диспетчер). Кроме момента начала взлета
-//и посадки(высота и скорость равны нулю)
-//• При попытке превысить максимальную скорость
-//диспетчер штрафует пилота на 100 очков и требует
-//немедленно снизить скорость.
+
--- Airplane pilot simulator/24-7/Start.cs
+++ Airplane pilot simulator/24-7/Start.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 using System.Timers;
 
@@ -9,41 +10,41 @@ namespace _24_7
 {
     class Start
     {
-        enum Direction { UP, RIGHT, DOWN, LEFT, UP_SHIFT, RIGHT_SHIFT, DOWN_SHIFT, LEFT_SHIFT,NONE };
+        enum Direction { UP, RIGHT, DOWN, LEFT, UP_SHIFT, RIGHT_SHIFT, DOWN_SHIFT, LEFT_SHIFT, NONE };
         static Direction d = Direction.NONE;
         static Aircraft Pilot = new Aircraft();
 
         private static void OnTimer(object sender, ElapsedEventArgs arg /* Предоставляет данные для события Elapsed */)
         {
-          
+
             switch (d)
             {
-                case Direction.LEFT_SHIFT:                 
-                        Pilot.Speed -= 150;
+                case Direction.LEFT_SHIFT:
+                    Pilot.Speed -= 150;
                     break;
                 case Direction.LEFT:
-                        Pilot.Speed -= 50;
+                    Pilot.Speed -= 50;
                     break;
 
-                case Direction.RIGHT_SHIFT:                
-                        Pilot.Speed += 150;
+                case Direction.RIGHT_SHIFT:
+                    Pilot.Speed += 150;
                     break;
                 case Direction.RIGHT:
-                        Pilot.Speed += 50;
+                    Pilot.Speed += 50;
                     break;
 
-                case Direction.UP_SHIFT:                    
-                        Pilot.Height += 500;
+                case Direction.UP_SHIFT:
+                    Pilot.Height += 500;
                     break;
                 case Direction.UP:
-                        Pilot.Height += 250;
+                    Pilot.Height += 250;
                     break;
 
-                case Direction.DOWN_SHIFT:                   
-                        Pilot.Height -= 500;
+                case Direction.DOWN_SHIFT:
+                    Pilot.Height -= 500;
                     break;
                 case Direction.DOWN:
-                        Pilot.Height -= 250;
+                    Pilot.Height -= 250;
                     break;
             }
             d = Direction.NONE;
@@ -57,10 +58,10 @@ namespace _24_7
         {
             Console.WriteLine("1. Add disp\n2. Dell disp\nEsc. Exit");
         }
-        static void Info_Air(int speed,int height)
+        static void Info_Air(int speed, int height)
         {
-            Console.WriteLine("Speed: {0}\nHeight: {1}",speed,height);
-        }      
+            Console.WriteLine("Speed: {0}\nHeight: {1}", speed, height);
+        }
         public static void Main()
         {
             int ball = 0;
@@ -70,121 +71,143 @@ namespace _24_7
 
 
             Console.TreatControlCAsInput = true;
-            
+
 
             Timer t = new Timer();
-            t.Interval = 100;       
+            t.Interval = 100;
             t.Elapsed += new ElapsedEventHandler(OnTimer);
 
             ConsoleKey key;
             do
             {
-              
                 ConsoleKeyInfo cki = Console.ReadKey();
                 key = cki.Key;
-
-               
-                if (menu)
+                try
                 {
-                    switch (key)
+                    if (menu)
                     {
-                        case ConsoleKey.D1:
+                        switch (key)
+                        {
+                            case ConsoleKey.D1:
+                                Console.Clear();
+                                Console.Write("Please, enter name ferst Dispatcher:");
+                                string disp = (Console.ReadLine());
+                                dispatchers.Add(new Dispatcher(disp));
 
-                            Console.Write("Please, enter name ferst Dispatcher:");
-                            string disp = (Console.ReadLine());
-                            dispatchers.Add(new Dispatcher(disp));
+                                Console.Write("Please, enter name second Dispatcher:");
+                                disp = (Console.ReadLine());
+                                dispatchers.Add(new Dispatcher(disp));
 
-                            Console.Write("Please, enter name second Dispatcher:");
-                             disp = (Console.ReadLine());
-                            dispatchers.Add(new Dispatcher(disp));
+                                Pilot.Observation += (new Dis(Disp));
 
-                            Pilot.Observation += (new Dis(Disp));
+                                menu = !menu;
 
-                            menu = !menu;
+                                Console.Clear();
+                                Print_Work_Menu();
+                                t.Start(); // Начинает вызывать событие Elapsed
+                                break;
+                            default:
+                                Console.Clear();
+                                Print_Start_Menu();
+                                break;
+                        }
+                    }
+                    else
+                    {
 
-                            Console.Clear();
-                            Print_Work_Menu();
-                            t.Start(); // Начинает вызывать событие Elapsed
-                            break;
-                        default:
-                            Console.Clear();
-                            Print_Start_Menu();
-                            break;
+                        switch (key)
+                        {
+                            case ConsoleKey.D1:
+                                t.Stop();
+                                Console.Clear();
+                                Console.Write("Please, enter name dispatcher:");
+                                string name = Console.ReadLine();
+                                dispatchers.Add(new Dispatcher(name));
+                                t.Start();
+                                break;
+                            case ConsoleKey.D2:
+                                t.Stop();
+                                if (dispatchers.Count > 2)
+                                {
+                                    for (int i = 0; i < dispatchers.Count; i++)
+                                    {
+                                        Console.WriteLine(i + ". " + dispatchers[i].Name);
+                                    }
+                                    Console.Write("Please, enter number dispatcher for dell:");
+                                   
+                                    int number= Int32.Parse(Console.ReadLine());
+
+                                    if (number < dispatchers.Count)
+                                    {
+                                        ball += dispatchers[number].Score;
+                                        dispatchers.Remove(dispatchers[number]);
+                                    }
+                                   
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Min 2 disp, please first add disp.");
+                                }
+                                t.Start();
+                                break;
+
+                            case ConsoleKey.LeftArrow:
+                                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                                    d = Direction.LEFT_SHIFT;
+                                else
+                                    d = Direction.LEFT;
+                                break;
+                            case ConsoleKey.RightArrow:
+                                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                                    d = Direction.RIGHT_SHIFT;
+                                else
+                                    d = Direction.RIGHT;
+                                break;
+                            case ConsoleKey.UpArrow:
+                                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                                    d = Direction.UP_SHIFT;
+                                else
+                                    d = Direction.UP;
+                                break;
+                            case ConsoleKey.DownArrow:
+                                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                                    d = Direction.DOWN_SHIFT;
+                                else
+                                    d = Direction.DOWN;
+                                break;
+                        }
+
+                        Console.Clear();
+                        Print_Work_Menu();
+
+                        foreach (Dispatcher i in dispatchers)
+                            Pilot.Generator_Event_Observation(i);
+
+                        Info_Air(Pilot.Speed, Pilot.Height);
                     }
                 }
-                else
+                catch (Exception ex)
                 {
-
-                    switch (key)
-                    {
-                        case ConsoleKey.D1:
-                            Console.Write("Please, enter name dispatcher:");
-                            string name = Console.ReadLine();
-                            dispatchers.Add(new Dispatcher(name));
-                            break;
-                        case ConsoleKey.D2:
-                            if (dispatchers.Count > 2)
-                            {
-                                for (int i = 0; i < dispatchers.Count; i++)
-                                {
-                                    Console.WriteLine(i + ". " + dispatchers[i].Name);
-                                }
-                                Console.Write("Please, enter number dispatcher for dell:");
-
-                                int number = Int32.Parse(Console.ReadLine());
-
-                                if (number < dispatchers.Count)
-                                {
-                                    ball += dispatchers[number].Score;
-                                    dispatchers.Remove(dispatchers[number]);
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Min 2 disp, please first add disp.");
-                            }
-                            break;
-
-                        case ConsoleKey.LeftArrow:
-                            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
-                                d = Direction.LEFT_SHIFT;                           
-                            else
-                                d = Direction.LEFT;
-                            break;
-                        case ConsoleKey.RightArrow:
-                            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
-                                d = Direction.RIGHT_SHIFT;
-                            else
-                                d = Direction.RIGHT;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
-                                d = Direction.UP_SHIFT;
-                            else
-                                d = Direction.UP;
-                            break;
-                        case ConsoleKey.DownArrow:
-                            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
-                                d = Direction.DOWN_SHIFT;
-                            else
-                                d = Direction.DOWN;
-                            break;
-                    }
-
+                    t.Stop();
                     Console.Clear();
-                    Print_Work_Menu();
+                    Console.WriteLine(ex.Message);
+                    menu = true;
+
                     foreach (Dispatcher i in dispatchers)
-                        Pilot.Generator_Event_Observation(i);
-                    Info_Air(Pilot.Speed, Pilot.Height);
+                        ball += i.Score;
+
+                    Console.WriteLine("You score:{0}", ball);
+                    ball = 0;
+                    dispatchers.Clear();
                 }
-            } while (key!= ConsoleKey.Escape);
+            } while (key != ConsoleKey.Escape);
+
         }
 
-        private static void Disp(object air,object disp)
+        private static void Disp(object air, object disp)
         {
-            ((Dispatcher)disp).Print(((Aircraft)air).Speed, ((Aircraft)air).Height);
+            ((Dispatcher)disp).Flight_check(((Aircraft)air).Speed, ((Aircraft)air).Height);
         }
 
     }
 }
-//Hp=7*Скорость (км/ч) – N,
